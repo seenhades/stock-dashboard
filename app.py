b952c038badf6ccec97b5bfa29fbc7aa9cceae9b
seenhades <seen0422@gmail.com>
2025-05-31 20:09:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ stock_list = {
     "Organon & Co.": "OGN",
     "Newmont Corporation": "NEM",
     "Infineon Twchnologies AG": "IFX.DE",
-    "Pocsche Automobil Holding SE: "PAH3.DE"
+    "Porsche Automobil Holding SE: "PAH3.DE"
     "Shell plc": "SHEL.L",
     "1306 Next Funds Topix Exchange Trade Fund": "1306.T",
     "Panasonic Holdings Corporation": "6752.T",
